c3c1ecfa76e20128f3b49c694d5f7a053a4647d5
Aditya <adityapattani@gmail.com>
2017-06-18 01:11:32 +0530

    Commit

--- app/src/main/java/com/example/aditya/profiler/LoginActivity.java
+++ app/src/main/java/com/example/aditya/profiler/LoginActivity.java
@@ -1,13 +1,37 @@
 package com.example.aditya.profiler;
 
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
+
+    EditText email, passwd;
+    TextView signup;
+    Button login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        setTitle("Login");
+
+        email = (EditText) findViewById(R.id.email);
+        passwd = (EditText) findViewById(R.id.password);
+        login = (Button) findViewById(R.id.loginbtn);
+        signup = (TextView) findViewById(R.id.signup);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String emailstr = email.getText().toString().trim();
+                String passwdstr = passwd.getText().toString().trim();
+            }
+        });
     }
 }
