8d7f5591e299b9bc723767e38bebcedcf73ba606
Aditya <adityapattani@gmail.com>
2017-06-29 21:03:55 +0530

    29-Jun-17

--- app/src/main/java/com/example/aditya/profiler/History.java
+++ app/src/main/java/com/example/aditya/profiler/History.java
@@ -1,11 +1,15 @@
 package com.example.aditya.profiler;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -15,7 +19,10 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 public class History extends Fragment {
@@ -37,8 +44,10 @@ public class History extends Fragment {
     public void onResume() {
         super.onResume();
 
+        history.clear();
+
         FirebaseAuth mAuth = FirebaseAuth.getInstance();
-        String userID = mAuth.getCurrentUser().getUid();
+        final String userID = mAuth.getCurrentUser().getUid();
 
         FirebaseDatabase.getInstance().getReference("history").child(userID).addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
@@ -68,6 +77,24 @@ public class History extends Fragment {
                 }
                 final ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(getActivity().getApplicationContext(), R.layout.list_item, history);
                 listView.setAdapter(arrayAdapter);
+
+                listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                        String intentName = history.get(position);
+                        String arr[] = intentName.split("on");
+                        intentName = arr[0].trim();
+
+                        DateFormat dateFormat = new SimpleDateFormat("yyyyMMdd HH:mm:ss");
+                        Date date = new Date();
+
+                        FirebaseDatabase.getInstance().getReference("history").child(userID).child(dateFormat.format(date).toString()).setValue(intentName);
+
+                        Intent intent = new Intent(getActivity().getApplicationContext(), ProfileActivity.class);
+                        intent.putExtra("Name", intentName);
+                        startActivity(intent);
+                    }
+                });
             }
 
             @Override
